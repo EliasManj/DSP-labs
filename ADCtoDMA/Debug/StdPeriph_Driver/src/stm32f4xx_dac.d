252de01c35a3a53c6a7bac062c1ab9950c9ae906
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-10-07 20:53:38 -0500

    adc dma example

---
+++ ADCtoDMA/Debug/StdPeriph_Driver/src/stm32f4xx_dac.d
@@ -0,0 +1,107 @@
+StdPeriph_Driver/src/stm32f4xx_dac.o: \
+ ../StdPeriph_Driver/src/stm32f4xx_dac.c \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/device/stm32f4xx.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cm4.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cmInstr.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cmFunc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cm4_simd.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/misc.h \
+ C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rcc.h
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/device/stm32f4xx.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cm4.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cmInstr.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cmFunc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/core/core_cm4_simd.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/Elias\ g/stm32/ADCtoDMA/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
---
+++ ADCtoDMA/src/main.c
@@ -0,0 +1,206 @@
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_discovery.h"
+#include "CustomDefines.h"
+#include "stm32f4xx_conf.h"
+
+/** @addtogroup STM32F4_Discovery_Peripheral_Examples
+  * @{
+  */
+
+/** @addtogroup ADC_ADC3_DMA
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define ADC3_DR_ADDRESS    (uint32_t)(&(ADC3->DR))//
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+__IO uint8_t ADC3ConvertedValue[BUFFER_SIZE*2]; //Array for ADC samples
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+void ADC3_CH12_DMA_Config(void);
+void UART_Config(void);
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+
+  UART_Config(); //Configure UART
+  ADC3_CH12_DMA_Config(); //Configure ADC and DMA
+  ADC_Cmd(ADC3, ENABLE); //Enable ADC
+
+
+  /* Start ADC3 Software Conversion */
+  ADC_SoftwareStartConv(ADC3);
+  while (1)
+  {
+  }
+}
+
+
+
+  /* @brief  ADC3 channel12 with DMA configuration*/
+
+void ADC3_CH12_DMA_Config(void)
+{
+ /* Initialization structures *******************************************************/
+  ADC_InitTypeDef       ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+  DMA_InitTypeDef       DMA_InitStructure;
+  GPIO_InitTypeDef      GPIO_InitStructure;
+  NVIC_InitTypeDef   NVIC_InitStructure;
+
+  /* Enable ADC3, DMA2 and GPIO clocks ****************************************/
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2 | RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3, ENABLE);
+
+  /* DMA2 Stream0 channel2 configuration **************************************/
+  DMA_InitStructure.DMA_Channel = DMA_Channel_2;
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)ADC3_DR_ADDRESS;
+  DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)(&ADC3ConvertedValue[0]);
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
+  DMA_InitStructure.DMA_BufferSize = BUFFER_SIZE;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+  DMA_Init(DMA2_Stream0, &DMA_InitStructure);
+  DMA_Cmd(DMA2_Stream0, ENABLE);
+  //Interrupt on transfer complete enable
+  DMA_ITConfig(DMA2_Stream0, DMA_IT_TC, ENABLE);
+
+  /* Configure ADC3 Channel12 pin as analog input ******************************/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  /* ADC Common Init **********************************************************/
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div4;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_15Cycles;
+  ADC_CommonInit(&ADC_CommonInitStructure);
+
+  /* ADC3 Init ****************************************************************/
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfConversion = 1;
+  ADC_Init(ADC3, &ADC_InitStructure);
+
+  /* ADC3 regular channel12 configuration *************************************/
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_12, 1, ADC_SampleTime_3Cycles);
+
+ /* Enable DMA request after last transfer (Single-ADC mode) */
+  ADC_DMARequestAfterLastTransferCmd(ADC3, ENABLE);
+
+  /* Configure Interrupt */
+  /* Enable and set DMA Interrupt to the lowest priority */
+  NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  /* Enable ADC3 DMA */
+  ADC_DMACmd(ADC3, ENABLE);
+}
+void UART_Config(void)
+{
+	USART_InitTypeDef	USART_InitStruct;
+	NVIC_InitTypeDef	NVIC_InitStruct;
+	GPIO_InitTypeDef	GPIO_InitStruct;
+
+	// Enable clock for GPIOA
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+	/**
+	* Tell pins PA9 and PA10 which alternating function you will use
+	* @important Make sure, these lines are before pins configuration!
+	*/
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+	// Initialize pins as alternating function
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/**
+	 * Enable clock for USART1 peripheral
+	 */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/**
+	 * Set Baudrate to value you pass to function
+	 * Disable Hardware Flow control
+	 * Set Mode To TX and RX, so USART will work in full-duplex mode
+	 * Disable parity bit
+	 * Set 1 stop bit
+	 * Set Data bits to 8
+	 *
+	 * Initialize USART1
+	 * Activate USART1
+	 */
+	USART_InitStruct.USART_BaudRate = BAUDRATE;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_Init(USART1, &USART_InitStruct);
+	USART_Cmd(USART1, ENABLE);
+
+	/**
+	 * Enable RX interrupt
+	 */
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+	/**
+	 * Set Channel to USART1
+	 * Set Channel Cmd to enable. That will enable USART1 channel in NVIC
+	 * Set Both priorities to 0. This means high priority
+	 *
+	 * Initialize NVIC
+	 */
+	NVIC_InitStruct.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	NVIC_Init(&NVIC_InitStruct);
+
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
